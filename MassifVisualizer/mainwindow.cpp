adb9277e76f9333020ed01470767d29fdd55e042
Student <petar.b.zecevic@gmail.com>
2021-03-03 21:30:23 +0100

    created a part of the toolbar

--- MassifVisualizer/mainwindow.cpp
+++ MassifVisualizer/mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <iostream>
+
+//needed for reading from a file
+#include <fstream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +17,34 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionOpen_File_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+                              "Massif Out File (rainbow emoji)", "./", "massif.out.*");
+
+    if (fileName.isEmpty())
+        return;
+
+    _fileName = fileName.toStdString();
+
+    //TODO: parse the input
+
+    //template for reading from a file
+   /* std::ifstream in(fileName.toStdString());
+    char c;
+    while (in >> c) {
+        std::cout << c;
+    }
+    std::cout << std::endl;*/
+
+
+    //TODO: visualize the data
+
+}
+
+void MainWindow::on_actionQuit_triggered()
+{
+     QApplication::quit();
 }
--- MassifVisualizer/mainwindow.h
+++ MassifVisualizer/mainwindow.h
@@ -15,8 +15,14 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionOpen_File_triggered();
+
+    void on_actionQuit_triggered();
+
 private:
     Ui::MainWindow *ui;
+    std::string _fileName;
 };
 
 #endif // MAINWINDOW_H
