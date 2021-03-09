b9f561f7a14b0a6cb5948fa86e82e6db245e320f
mandja96 <mandja96@gmail.com>
2021-03-09 14:56:17 +0100

    implemented parseMassifOutput function

--- MassifVisualizer/parse-massif-file/parser-massif.cpp
+++ MassifVisualizer/parse-massif-file/parser-massif.cpp
@@ -11,14 +11,87 @@ ParserMassif::ParserMassif(std::string inputFileName)
 
 }
 
+/*
+    desc: --massif-out-file=izlaz_masif
+    cmd: ./a.out
+    time_unit: i
+    #-----------
+    snapshot=0
+    #-----------
+    time=0
+    mem_heap_B=0
+    mem_heap_extra_B=0
+    mem_stacks_B=0
+    heap_tree=empty/peak/detailed
+*/
 void ParserMassif::parseMassifOutput()
 {
     std::string line;
+    std::vector<std::string> lines;
     std::ifstream inputFile(_inputFileName);
+
     if (inputFile.is_open()) {
+        std::getline(inputFile, line);
+        parseDescLine(line);
+        std::getline(inputFile, line);
+        parseCmdLine(line);
+        std::getline(inputFile, line);
+        parseTimeUnitLine(line);
+
+        // to swallow this: #-----------
+        std::getline(inputFile, line);
+
         while (std::getline(inputFile, line)) {
-            std::cout << line << '\n';
+            //std::cout << line << '\n';
+            if (!line.compare(0, 8, "snapshot")) {
+                // to extract snapshot=#
+                parseSnapshotNumberLine(line);
+                // to swallow this: #-----------
+                std::getline(inputFile, line);
+
+                // to extract time=#
+                std::getline(inputFile, line);
+                parseTimeValueLine(line);
+
+                // to extract mem_heap_B=#
+                std::getline(inputFile, line);
+                parseMemHeapBLine(line);
+
+                // to extract mem_heap_extra_B=#
+                std::getline(inputFile, line);
+                parseMemHeapExtraBLine(line);
+
+                // to extract mem_stacks_B=#
+                std::getline(inputFile, line);
+                parseMemStacksBLine(line);
+
+                // to extract heap_tree
+                std::getline(inputFile, line);
+                if (!line.compare("heap_tree=empty")) {
+                    std::cout << "Heap tree is empty here." << std::endl;
+                    // to swallow this: #-----------
+                    std::getline(inputFile, line);
+                }
+                else {
+                    std::cout << "Heap tree is not empty here." << std::endl;
+                    std::cout << line << std::endl;
+
+                    lines.empty();
+                    /*
+                     * push_back("foo") constructs a temporary string from the string literal,
+                     * and then moves that string into the container, whereas emplace_back("foo")
+                     * just constructs the string directly in the container,
+                     * avoiding the extra move.
+                    */
+                    while (line.compare("#-----------") && !inputFile.eof()) {
+                        lines.emplace_back(line);
+                        std::getline(inputFile, line);
+                    }
+                    parseHeapTreeLines(lines);
+                }
+            }
         }
+        inputFile.close();
     }
     else {
         std::cout << "File is not opened." << std::endl;
