acf9ccd156f691f872dcc55ed1a58dad6c2f93cb
mandja96 <mandja96@gmail.com>
2021-03-09 16:16:08 +0100

    added fields to the HeapTreeItem class

--- MassifVisualizer/parse-massif-file/heaptreeitem.cpp
+++ MassifVisualizer/parse-massif-file/heaptreeitem.cpp
@@ -1,6 +1,18 @@
 #include "heaptreeitem.h"
 
 HeapTreeItem::HeapTreeItem()
+    : _numOfDirectChildren(0),
+      _memoryAlloc(0),
+      _fileName(""),
+      _funcName(""),
+      _lineNum(0),
+      _mother(nullptr)
 {
 
 }
+HeapTreeItem::~HeapTreeItem()
+{
+    //TODO: delete all children and test that
+}
+
+//TODO: define functions for inserting child and setting parent
--- MassifVisualizer/parse-massif-file/heaptreeitem.h
+++ MassifVisualizer/parse-massif-file/heaptreeitem.h
@@ -1,11 +1,24 @@
 #ifndef HEAPTREEITEM_H
 #define HEAPTREEITEM_H
 
+#include <qglobal.h>
+#include <string>
+#include <vector>
 
 class HeapTreeItem
 {
 public:
     HeapTreeItem();
+    ~HeapTreeItem();
+
+private:
+    uint _numOfDirectChildren;
+    quint64 _memoryAlloc;
+    std::string _fileName;
+    std::string _funcName;
+    uint _lineNum;
+    std::vector<HeapTreeItem*> _children;
+    HeapTreeItem* _mother;
 };
 
 #endif // HEAPTREEITEM_H
