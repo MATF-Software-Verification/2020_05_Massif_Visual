280cb126abc0e5bd3c0a830011bbc5c419a3e35d
mandja96 <mandja96@gmail.com>
2021-04-05 01:08:18 +0200

    deleted one comment from main.cpp

--- MassifVisualizer/main.cpp
+++ MassifVisualizer/main.cpp
@@ -10,14 +10,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-//    TODO: figure out how to execute valgrind from c++
-//    system("valgrind --tool=massif --massif-out-file=opala /home/student/Desktop/a.out 2>&0");
-
-//    std::cout << "OUTPUT=$(valgrind --tool=massif ./a.out 2>&1) | echo $OUTPUT | grep -o ==[0-9][0-9]* | head -1 | cut -c3-";
-//    std::regex rgx("==([0-9]+)==");
-//    std::smatch match;
-//    if (std::regex_search(terminalOutputValgrind.begin(), s.end(), match, rgx))
-//        std::cout << "match: " << match[1] << '\n';
-//    std::cout << system("ls -ct -1 | grep \"massif\\.out\\.\" | head -1") << std::endl;
     return a.exec();
 }
